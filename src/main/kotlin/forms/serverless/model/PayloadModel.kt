6fcfbc82274908eabaf7259923f62f80ddd6697f
yajneshvar <yajneshvar.arumugam@gmail.com>
2021-05-01 22:41:08 -0400

    Add new field additional notes Add to email template

--- src/main/kotlin/forms/serverless/model/PayloadModel.kt
+++ src/main/kotlin/forms/serverless/model/PayloadModel.kt
@@ -1,14 +1,16 @@
 package forms.serverless.model
 
-data class Order(var createdDate: String?, var orderId: String?, val customerId: String, val type: String = "Sale",  val books: List<BookQuantity>, val channel: String, val delivery: Boolean, val deliveryNotes: String?, val paymentNotes: String?, val creator: String)
+data class Order(var createdDate: String?, var orderId: String?, val customerId: String, val type: String = "Sale",  val books: List<BookQuantity>, val channel: String, val delivery: Boolean, val deliveryNotes: String?, val paymentNotes: String?, val additionalNotes: String?, val creator: String)
 
 fun Order.toList(): List<List<String>> {
     return books.map {
-        listOf(this.createdDate!!, this.orderId!!, this.customerId, this.type, it.code, it.title, it.quantity.toString(), channel, delivery.toString(), deliveryNotes.orEmpty(), paymentNotes.orEmpty(), creator)
+        listOf(this.createdDate!!, this.orderId!!, this.customerId, this.type, it.code, it.title, it.quantity.toString(), channel, delivery.toString(), deliveryNotes.orEmpty(), paymentNotes ?: additionalNotes.orEmpty(), creator)
     }
 }
 
 fun Order.toEmailText(): String {
+    val deliveryText = if (this.delivery)  "Delivery required for this order" else  "Delivery not required for this order"
+    val deliveryNotesText = if (delivery) this.deliveryNotes else ""
     return """
         Successfully created Order for ${this.customerId}
         Distributor: ${this.creator}
@@ -17,7 +19,8 @@ fun Order.toEmailText(): String {
         Order Info:
         ${books.map { "${it.title} : ${it.quantity}" }.joinToString(separator="\n")}
         Payment Notes: ${this.paymentNotes}
-        Delivery Notes: ${this.deliveryNotes}
+        $deliveryText
+        $deliveryNotesText
     """.trimIndent()
 }
 
